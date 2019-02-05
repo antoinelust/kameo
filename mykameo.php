e58bbc3cf3a48016e038d92c41e9d2130eda62de
Antoine Lust <antoine.lust@hotmail.fr>
2019-02-05 21:01:36 +0100

    modification pour avoir un pop-up pour large

--- mykameo.php
+++ mykameo.php
@@ -2554,7 +2554,7 @@ if($connected){
 </div>
 
 <div class="modal fade" id="ReservationsListing" tabindex="-1" role="modal" aria-labelledby="modal-label" aria-hidden="true" style="display: none;">
-	<div class="modal-dialog">
+	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
