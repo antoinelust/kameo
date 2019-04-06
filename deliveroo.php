636ae1dd9afe8d5ee9d951270ba9bb16712d0792
Thibaut <thibaut.mativa@gmail.com>
2019-04-06 14:46:22 +0200

    nouvelle page pour deliveroo

---
+++ deliveroo.php
@@ -0,0 +1,190 @@
+<?php 
+include 'include/header3.php';
+?>
+		
+		<!--Square icons-->
+		
+  
+   <section>
+	
+	<div class="container">
+		<div class="row">	
+					
+				<div class="heading heading text-left m-b-20">
+					<h2 class="fr">Catalogue</h2>
+					<h2 class="en">Catalog</h2>
+					<h2 class="nl">Catalogus</h2>
+				</div>
+				<br>
+            
+				<p class="fr">KAMEO Bikes se met un point d'honneur à fournir des vélos de qualité. C'est pourquoi nous ne travaillons qu'avec des partenaires sérieux, qui proposent des produits à la hauteur de vos attentes. Veuillez trouver ci-dessous la liste de nos partenaires.</p>
+				<p class="en">KAMEO Bikes focuses on high quality bikes. That's why we only work with premium suppliers implanted in Europe. Please find here below the list of such.</p>
+				<p class="nl">KAMEO Bikes richt zich op fietsen van hoge kwaliteit. Dat is de reden waarom wij alleen werken met premium leveranciers geïmplanteerd in Europa. Hieronder vindt u de lijst met dergelijke.</p>
+            
+            	<!--
+				<p class="fr">KAMEO Bikes propose également un grand nombre d'accessoires pour compléter votre solution de mobilité.<br /><a class="button small green button-3d rounded effect icon-left" href="accessoire.php"><span><i class="fa fa-eye"></i>Découvrez le catalogue d'accessoires</span></a></p>
+				<p class="en">KAMEO Bikes also offers a wide range of accessories to complete your mobility solution.<br /><a class="button small green button-3d rounded effect icon-left" href="accessoire.php"><span><i class="fa fa-eye"></i>Discover our accessories catalogue</span></a></p>
+				<p class="nl">KAMEO Bikes biedt ook een breed scala aan accessoires om uw mobiliteitsoplossing te voltooien.<br /><a class="button small green button-3d rounded effect icon-left" href="accessoire.php"><span><i class="fa fa-eye"></i>Ontdek onze accessoirescatalogus</span></a></p>
+			-->
+		<!--	
+		</div>
+	</div>
+	</section>
+		-->
+	<div class="separator"></div>
+	
+				<h3 class="fr">Conway ets 200 SE</h3> 
+				<h3 class="en">Conway ets 200 SE</h3> 
+				<h3 class="nl">Conway ets 200 SE</h3> 
+				<br>
+				<div class="col-md-4">
+                     <h4 class="text-green">Prix d'achat</h4>
+                     <p>2100€ TVAC</p>
+                     <h4 class="text-green">Prix leasing</h4>
+                     <p>95€ TVAC / mois</p>
+                     <p>Valeur de rachat du leasing<br>
+                     315€ TVAC</p>
+                     <h4 class="text-green">Prix location</h4>
+                     <p>110€ TVAC / mois</p>
+                     <p>Prix par km au delà de 600 km/mois<br>
+                     0,23€</p>
+                     <br>
+                     <a href="https://www.conway-bikes.de/modell/ets-200-se/" target="_blank"><i class="fa fa-plus-square"></i> Plus d'informations</a>
+                     <br>
+                     <br>
+                     
+					<div class="col-md-8">
+					<a class="button green button-3d rounded effect icon-left fr" href="#"><span><i class="fa fa-shopping-cart"></i>Commander</span></a>
+					<a class="button green button-3d rounded effect icon-left en" href="#"><span><i class="fa fa-shopping-cart"></i>Commander</span></a>
+					<a class="button green button-3d rounded effect icon-left nl" href="#"><span><i class="fa fa-shopping-cart"></i>Commander</span></a>
+					</div>
+					
+				</div>
+				
+				<img class="col-md-8" src="images/deliveroo/Conway_ets200SE.jpg" alt="Conway ets 200 SE">
+				
+								
+				<div class="separator"></div>
+				
+				<img class="col-md-8" src="images/deliveroo/Conway_ets300.jpg" alt="Conway ets 300">
+				
+				<h3 class="fr">Conway ets 300</h3> 
+				<h3 class="en">Conway ets 300</h3> 
+				<h3 class="nl">Conway ets 300</h3> 
+				<br>
+				<div class="col-md-4">
+					 <h4 class="text-green">Prix d'achat</h4>
+                     <p>2500€ TVAC</p>
+                     <h4 class="text-green">Prix leasing</h4>
+                     <p>110€ TVAC / mois</p>
+                     <p>Valeur de rachat du leasing<br>
+                     375€ TVAC</p>
+                     <h4 class="text-green">Prix location</h4>
+                     <p>127€ TVAC / mois</p>
+                     <p>Prix par km au delà de 600 km/mois<br>
+                     0,27€</p>
+                     <br>
+                     <a href="https://www.conway-bikes.de/modell/ets-300/" target="_blank"><i class="fa fa-plus-square"></i> Plus d'informations</a>
+                     <br>
+                     <br>
+					<div class="col-md-8">
+					<a class="button green button-3d rounded effect icon-left fr" href="#"><span><i class="fa fa-shopping-cart"></i>Commander</span></a>
+					<a class="button green button-3d rounded effect icon-left en" href="#"><span><i class="fa fa-shopping-cart"></i>Commander</span></a>
+					<a class="button green button-3d rounded effect icon-left nl" href="#"><span><i class="fa fa-shopping-cart"></i>Commander</span></a>
+					</div>
+					
+				</div>
+				
+								
+				<div class="separator"></div>
+				
+				<h3 class="fr">Victoria e special 10.7 (45 km/h)</h3> 
+				<h3 class="en">Victoria e special 10.7 (45 km/h)</h3> 
+				<h3 class="nl">Victoria e special 10.7 (45 km/h)</h3> 
+				<br>
+				<div class="col-md-4">
+					<h4>Caractéristiques techniques :</h4>
+					 <h4 class="text-green">Prix d'achat</h4>
+                     <p>3700€ TVAC</p>
+                     <h4 class="text-green">Prix leasing</h4>
+                     <p>145€ TVAC / mois</p>
+                     <p>Valeur de rachat du leasing<br>
+                     555€ TVAC</p>
+                     <h4 class="text-green">Prix location</h4>
+                     <p>167€ TVAC / mois</p>
+                     <p>Prix par km au delà de 600 km/mois<br>
+                     0,33€</p>
+                     <br>
+                     <a href="https://www.victoria-fahrrad.de/en/modell/victoria-espezial-10-7" target="_blank"><i class="fa fa-plus-square"></i> Plus d'informations</a>
+                     <br>
+                     <br>
+					<div class="col-md-8">
+					<a class="button green button-3d rounded effect icon-left fr" href="#"><span><i class="fa fa-shopping-cart"></i>Commander</span></a>
+					<a class="button green button-3d rounded effect icon-left en" href="#"><span><i class="fa fa-shopping-cart"></i>Commander</span></a>
+					<a class="button green button-3d rounded effect icon-left nl" href="#"><span><i class="fa fa-shopping-cart"></i>Commander</span></a>
+					</div>
+					
+				</div>
+				
+				<img class="col-md-8" src="images/deliveroo/Victoria_especial.jpg" alt="Victoria e special 10.7 (45 km/h)">
+				
+								
+				<div class="separator"></div>
+				
+				<!--Portfolio Carousel -->
+
+
+				
+    </div>
+    </div>
+    <!--End: Square icons-->
+
+
+
+		
+		<!-- FOOTER -->
+	<footer class="background-dark text-grey" id="footer">
+    <div class="footer-content">
+        <div class="container">
+        
+        <br><br>
+        
+            <div class="row text-center">
+                <div class="copyright-text text-center"><ins>Kameo Bikes SPRL</ins> 
+					<br>BE 0681.879.712 
+					<br>+32 498 72 75 46 </div>
+					<br>
+                <div class="social-icons center">
+							<ul>
+								<li class="social-facebook"><a href="https://www.facebook.com/Kameo-Bikes-123406464990910/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								
+								<li class="social-instagram"><a href="https://www.instagram.com/kameobikes/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+				</div>
+				<!--
+				<div class="copyright-text text-center"><a href="blog.php" class="text-green text-bold">Le blog</a> | <a href="faq.php" class="text-green text-bold">FAQ</a></div>
+				-->
+				<br>
+				<br>
+				
+            </div>
+        </div>
+    </div>
+</footer>
+		<!-- END: FOOTER -->
+
+
+	</div>
+	<!-- END: WRAPPER -->
+
+	<!-- Theme Base, Components and Settings -->
+	<script src="js/theme-functions.js"></script>
+
+	<!-- Language management -->
+	<script type="text/javascript" src="js/language.js"></script>
+
+
+
+</body>
+
+</html>
