ccec8a5f69692b4cb1239a5a728e69d888ed7cba
Antoine Lust <antoine.lust@hotmail.fr>
2020-02-15 13:51:15 +0100

    feedback

--- mykameo.php
+++ mykameo.php
@@ -60,7 +60,6 @@ window.addEventListener("DOMContentLoaded", function(event) {
         classname[i].addEventListener('click', function () { get_company_listing('*')}, false);
         classname[i].addEventListener('click', function () { listPortfolioBikes()}, false);
         classname[i].addEventListener('click', function () { list_feedbacks()}, false);
-        
         classname[i].addEventListener('click', function () { list_bikes_admin()}, false);
         classname[i].addEventListener('click', function () { list_tasks('*', $('.taskOwnerSelection').val(), $('.tasksListing_number').val())}, false);
         classname[i].addEventListener('click', function () { generateTasksGraphic('*', $('.taskOwnerSelection2').val(), $('.numberOfDays').val())}, false);
@@ -185,208 +184,6 @@ function initiatizeFeedback(id){
             }
         }
     });
-}
-
-function list_feedbacks() {
-    $.ajax({
-        url: 'include/feedback_management.php',
-        type: 'get',
-        data: {action: "list"},
-        success: function(response){
-            if(response.response == 'error') {
-                console.log(response.message);
-            }
-            if(response.response == 'success'){
-
-                document.getElementById('counterFeedbacks').innerHTML = "<span data-speed=\"1\" data-refresh-interval=\"4\" data-to=\""+response.feedbacksNumber+"\" data-from=\"0\" data-seperator=\"true\">"+response.feedbacksNumber+"</span>";
-
-
-                var i=0;
-                var dest="";
-                var temp="<table class=\"table table-condensed\"><h4 class=\"fr-inline text-green\">Feedbacks:</h4><h4 class=\"en-inline text-green\">Feedbacks:</h4><h4 class=\"nl-inline text-green\">Feedbacks:</h4><br/><br/><div class=\"seperator seperator-small visible-xs\"></div><tbody><thead><tr><th>ID</th><th><span class=\"fr-inline\">Société</span><span class=\"en-inline\">Company</span><span class=\"nl-inline\">Company</span></th><th>Bike</th><th>Start</th><th>End</th><th><span class=\"fr-inline\">Note</span><span class=\"en-inline\">Note</span><span class=\"nl-inline\">Note</span></th><th><span class=\"fr-inline\">Commentaire</span><span class=\"en-inline\">Comment</span><span class=\"nl-inline\">Comment</span></th><th><span class=\"fr-inline\">Entretien</span><span class=\"en-inline\">Maintenance ?</span><span class=\"nl-inline\">Maintenance ?</span></th><th><span class=\"fr-inline\">E-mail</span><span class=\"en-inline\">E-mail</span><span class=\"nl-inline\">E-mail</span></th><th>Statut</th></tr></thead>";
-                dest=dest.concat(temp);
-                while (i < response.feedbacksNumber){
-                    var unix_timestamp = response.feedback[i].start;
-                    // Create a new JavaScript Date object based on the timestamp
-                    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-                    var date = new Date(unix_timestamp * 1000);
-                    //day from the timestamp
-                    var day = date.getDate();
-                    //month from the timestamp
-                    var month = date.getMonth();
-                    //year from the timestamp
-                    var year = date.getFullYear();
-                    // Hours part from the timestamp
-                    var hours = date.getHours();
-                    // Minutes part from the timestamp
-                    var minutes = "0" + date.getMinutes();
-                    // Seconds part from the timestamp
-                    var seconds = "0" + date.getSeconds();
-
-                    // Will display time in 10:30:23 format
-                    var formattedTimeStart = day +'/' + month + '/' + year + ' ' + hours + ':' + minutes.substr(-2);
-
-                    var unix_timestamp = response.feedback[i].end;
-                    // Create a new JavaScript Date object based on the timestamp
-                    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-                    var date = new Date(unix_timestamp * 1000);
-                    //day from the timestamp
-                    var day = date.getDate();
-                    //month from the timestamp
-                    var month = date.getMonth();
-                    //year from the timestamp
-                    var year = date.getFullYear();
-                    // Hours part from the timestamp
-                    var hours = date.getHours();
-                    // Minutes part from the timestamp
-                    var minutes = "0" + date.getMinutes();
-                    // Seconds part from the timestamp
-                    var seconds = "0" + date.getSeconds();
-
-                    // Will display time in 10:30:23 format
-                    var formattedTimeEnd = day +'/' + month + '/' + year + ' ' + hours + ':' + minutes.substr(-2);
-
-                    if(response.feedback[i].entretien==null){
-                        entretien="<span>N/A</span>";
-                    } else if (response.feedback[i].entretien=="0"){
-                        entretien="<span class=\"text-green\">Non</span>";
-                    }else{
-                        entretien="<span class=\"text-red\">Oui</span>";
-                    }
-
-
-                    if(response.feedback[i].comment== null){
-                        var comment = 'N/A';
-                    }else{
-                        var comment = response.feedback[i].comment.substr(0,20);
-                    }
-                        
-                    if(response.feedback[i].note== null){
-                        var note = 'N/A';
-                    }else{
-                        var note = response.feedback[i].note;
-                    }
-                                                
-                    var temp="<tr><td><a href=\"#\" class=\"text-green retrieveFeedback\" data-target=\"#feedbackManagement\" name=\""+response.feedback[i].IDReservation+"\" data-toggle=\"modal\">"+response.feedback[i].IDReservation+"</a></td><td>"+response.feedback[i].company+"</td><td>"+response.feedback[i].bikeNumber+"<td>"+formattedTimeStart+"</td><td>"+formattedTimeEnd+"</td><td>"+note+"</td><td>"+comment+"</td><td>"+entretien+"</td><td>"+response.feedback[i].firstName+" " +response.feedback[i].name+"</td><td>"+response.feedback[i].status+"</td></tr>";
-                    dest=dest.concat(temp);
-                    i++;
-
-                }
-                var temp="</tobdy></table>";
-                dest=dest.concat(temp);
-
-                document.getElementById('feedbacksListingSpan').innerHTML = dest;
-                $('.retrieveFeedback').click(function(){
-                    retrieve_feedback(this.name);
-                });
-
-            }
-
-            displayLanguage();
-        }
-    })
-}
-
-function retrieve_feedback(ID) {
-    $.ajax({
-        url: 'include/feedback_management.php',
-        type: 'get',
-        data: {"action": "retrieveFeedback", "ID": ID},
-        success: function(response){
-            if(response.response == 'error') {
-                console.log(response.message);
-            }
-            if(response.response == 'success'){
-
-                var unix_timestamp = response.start
-                // Create a new JavaScript Date object based on the timestamp
-                // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-                var date = new Date(unix_timestamp * 1000);
-                //day from the timestamp
-                var day = date.getDate();
-                //month from the timestamp
-                var month = date.getMonth();
-                //year from the timestamp
-                var year = date.getFullYear();
-                // Hours part from the timestamp
-                var hours = date.getHours();
-                // Minutes part from the timestamp
-                var minutes = "0" + date.getMinutes();
-                // Seconds part from the timestamp
-                var seconds = "0" + date.getSeconds();
-
-                // Will display time in 10:30:23 format
-                var formattedTimeStart = day +'/' + month + '/' + year + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-
-                var unix_timestamp = response.end
-                // Create a new JavaScript Date object based on the timestamp
-                // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-                var date = new Date(unix_timestamp * 1000);
-                //day from the timestamp
-                var day = date.getDate();
-                //month from the timestamp
-                var month = date.getMonth();
-                //year from the timestamp
-                var year = date.getFullYear();
-                // Hours part from the timestamp
-                var hours = date.getHours();
-                // Minutes part from the timestamp
-                var minutes = "0" + date.getMinutes();
-                // Seconds part from the timestamp
-                var seconds = "0" + date.getSeconds();
-
-                // Will display time in 10:30:23 format
-                var formattedTimeEnd = day +'/' + month + '/' + year + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-                $('.feedbackManagementTitle').html("Consulter un feedback");
-                $('#feedbackManagement input[name=bike]').val(response.bike);
-                $('#feedbackManagement input[name=startDate]').val(formattedTimeStart);
-                $('#feedbackManagement input[name=endDate]').val(formattedTimeEnd);
-                $('#feedbackManagement input[name=ID]').val(response.ID);
-                $('#feedbackManagement input[name=utilisateur]').val(response.email);
-                
-                $('#feedbackManagement select[name=note]').attr("readonly", true);                
-                
-                if(response.note==null){
-                    $('.spanNote').addClass("hidden");
-                    $('#feedbackManagement select[name=note]').val('5');                
-                }else{
-                    $('#feedbackManagement select[name=note]').val(response.note);                
-                    $('.spanNote').removeClass("hidden");
-                    
-                }
-                
-                $('#feedbackManagement textarea[name=comment]').attr("readonly", true);
-                if(response.comment==null){
-                    $('.textAreaComment').addClass("hidden");
-                }else{
-                    $('#feedbackManagement textarea[name=comment]').val(response.comment);
-                    $('.textAreaComment').removeClass("hidden");
-                    
-                }
-                document.getElementsByClassName("feedbackBikeImage")[0].src="images_bikes/"+response.bike+"_mini.jpg";
-
-                if(response.entretien==null){
-                    $('.spanEntretien').addClass("hidden");
-                }
-                else if(response.entretien=="1"){
-                    $('#feedbackManagement input[name=entretien]').prop("checked", true);
-                    $('.spanEntretien').removeClass("hidden");
-                    
-                }else{
-                    $('#feedbackManagement input[name=entretien]').prop("checked", false);
-                    $('.spanEntretien').removeClass("hidden");
-                    
-                }
-
-                $('.feedbackManagementSendButton').addClass('hidden');
-
-            }
-
-            displayLanguage();
-        }
-    })
 }
 
 
@@ -8713,7 +8510,8 @@ if($connected){
 
           </script>
           <script type="text/javascript" src="js/add_company_contact.js">
-
+            </script>
+          <script type="text/javascript" src="js/feedback_management.js">
           </script>
 
           <div class="col-sm-12" id="clientBikes">
@@ -9026,6 +8824,8 @@ if($connected){
 </div>
 
 
+
+
 <div class="modal fade" id="feedbackManagement" tabindex="-1" role="modal" aria-labelledby="modal-label" aria-hidden="true" style="display: none; overflow-y: auto !important;">
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
@@ -9173,6 +8973,8 @@ if($connected){
     });
 
 </script>
+
+
 
 <div class="modal fade" id="bikeManagement" tabindex="-1" role="modal" aria-labelledby="modal-label" aria-hidden="true" style="display: none;">
 	<div class="modal-dialog modal-lg">
