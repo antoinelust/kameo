53c85efd657dcb1a96185deed7529be12644c18a
Pyadant <63552475+Pyadant@users.noreply.github.com>
2020-04-18 17:00:42 +0200

    Update avantages.php
    Traduction & adaptations

--- avantages.php
+++ avantages.php
@@ -7,15 +7,23 @@ include 'include/header5.php';
 	
 	<div class="container">
 		<div class="row">
-				<h1 class="text-green">AVANTAGES</h1>
+				<h1 class="text-green" "fr">QUELS SONT VOS AVANTAGES ?</h1>
+				<h1 class="text-green" "en">WHAT ARE YOUR BENEFITS ?</h1>
+				<h1 class="text-green" "nl">WELKE ZIJN UW VOORDELEN ?</h1>
 				<br>
-				<h3>Employeur</h3>
+				<h3 class="fr">Comme Employeur :</h3>
+				<h3 class="en">As Employer: </h3>
+				<h3 class="nl">Als Werkgever:</h3>
 				<div class="row">
 			      <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-leaf"></i></a> </div>
-			          <h5>Entreprise durable.</h5>
-			          <p>Véhiculez une image verte de votre entreprise.</p>
+			          <h5 class="fr">Entreprise durable.</h5>
+			          <h5 class="en">Sustainable business.</h5>
+			          <h5 class="nl">Duurzaam bedrijf.</h5>
+			          <p class="fr">Déplacez-vous sans émissions !</p>
+			          <p class="en">C02-free displacements !</p>
+			          <p class="nl">C02-vrij verplaatsingen !</p>
 			          <br>
 			          <br>
 			        </div>
@@ -23,80 +31,122 @@ include 'include/header5.php';
 			      <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-money"></i></a> </div>
-			          <h5>Incitants fiscaux</h5>
-			          <p> - déduisez les coûts liés au vélo à 100%<br>
-			          - réduisez les coûts liés à la flotte de voiture, aux coûts de parking, etc</p>
+			          <h5 class="fr">Incitants fiscaux</h5>
+			          <h5 class="en">Tax incentives</h5>
+			          <h5 class="nl">Belastingvoordelen</h5>
+			          <p class="fr"> - déduisez à 100% les coûts liés au vélo<br>
+			          - réduisez les coûts liés aux infrastructures, à la flotte de voiture, aux coûts de parking, etc.</p>
+					  <p class="en"> - deduct 100% of the costs related to cycling<br>
+			          - Reduce infrastructure, fleet, parking, etc. costs</p>
+					  <p class="nl"> - trek 100% van de kosten in verband met fietsen af%<br>
+			          - Verminder de kosten van infrastructuur, wagenpark, parkeerkosten, enz.</p>
 			          <br>
 			        </div>
 			      </div>
 			      <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-diamond"></i></a> </div>
-			          <h5>Bien-être des employés</h5>
-			          <p>Offrez une meilleure santé à vos employés et moins de stress lié aux embouteillages.</p>
+			          <h5 class="fr">Bien-être des employés</h5>
+			          <h5 class="en">Employee well-being</h5>
+			          <h5 class="nl">Welzijn van werknemers</h5>
+			          <p class="fr">Offrez une meilleure santé à vos employés et moins de stress lié aux embouteillages.</p>
+			          <p class="en">Provide better health for your employees and less stress related to traffic jams.</p>
+			          <p class="nl">Zorg voor een betere gezondheid van uw werknemers en minder stress door files.</p>
 			        </div>
 			      </div>
 			       <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-user"></i></a> </div>
-			          <h5>Atout pour attirer de nouveaux talents.</h5>
-			          <p>Recrutez de nouveaux talents grâce à une image jeune et dynamique.</p>
+			          <h5 class="fr">Atout pour attirer de nouveaux talents.</h5>
+			          <h5 class="en">Asset to attract new talents.</h5>
+			          <h5 class="nl">Voordeel om nieuwe talenten aan te trekken.</h5>
+			          <p class="fr">Recrutez de nouveaux talents grâce à une image jeune et dynamique.</p>
+			          <p class="en">Recruit new talents thanks to a young and dynamic image.</p>
+			          <p class="nl">Werf nieuwe talenten aan dankzij een jong en dynamisch imago.</p>
 			          <br>
 			        </div>
 			      </div>
 			    </div>
 			    
-			    <h3>Employé</h3>
+			    <h3 class="fr">Comme Employé :</h3>
+			    <h3 class="en">As Employee:</h3>
+			    <h3 class="nl">Als Medewerker:</h3>
 				<div class="row">
 			      <div class="col-md-4">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-leaf"></i></a> </div>
-			          <h5>Respect de l'environnement.</h5>
-			          <p>Véhiculez l'image verte de votre entreprise.</p>
+			          <h5 class="fr">Respect de l'environnement.</h5>
+			          <h5 class="en">Respect the environment.</h5>
+			          <h5 class="nl">Respecteer het milieu.</h5>
+			          <p class="fr">Déplacez-vous sans émissions !</p>
+			          <p class="en">C02-free displacements !</p>
+			          <p class="nl">C02-vrij verplaatsingen !</p>
 			          <br>
 			        </div>
 			      </div>
 			      <div class="col-md-4">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-money"></i></a> </div>
-			          <h5>Incitants fiscaux</h5>
-			          <p>Percevez la prime vélo de 0,24€ par km.</p>
+			          <h5 class="fr">Incitants fiscaux</h5>
+			          <h5 class="en">Tax incentives</h5>
+			          <h5 class="nl">Belastingvoordelen</h5>
+			          <p class="fr">Percevez une prime vélo défiscalisée de 0,24€ par km.</p>
+			          <p class="en">Receive a tax free bicycle premium of € 0.24 per km.</p>
+			          <p class="nl">Ontvang een belastingvrij fietspremie van € 0,24 per km.</p>
 			          <br>
 			        </div>
 			      </div>
 			      <div class="col-md-4">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-clock-o"></i></a> </div>
-			          <h5>Gain de temps</h5>
-			          <p>Gagnez du temps sur votre trajet en évitant les embouteillages.</p>
+			          <h5 class="fr">Gain de temps</h5>
+			          <h5 class="en">Time saving</h5>
+			          <h5 class="nl">Tijd besparen</h5>
+			          <p class="fr">Pas d'embouteillages, c'est du temps en plus pour ce qui compte vraiment.</p>
+			          <p class="en">No traffic jams, it's extra time for what really matters.</p>
+			          <p class="nl">Geen files, het is extra tijd voor wat er echt toe doet.</p>
 			        </div>
 			      </div>
 			       <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-plus"></i></a> </div>
-			          <h5>Combinaison</h5>
-			          <p>Combinez la location tout inclus vélo à l'usage d'une voiture de société.</p>
+			          <h5 class="fr">Combinaison</h5>
+			          <h5 class="en">Combination</h5>
+			          <h5 class="nl">Combinatie</h5>
+			          <p class="fr">Combinez la location tout inclus vélo à l'usage d'une voiture de société.</p>
+			          <p class="en">Combine the all-inclusive bike rental with the use of a company car.</p>
+			          <p class="nl">Combineer de all-inclusive fietsverhuur met het gebruik van een bedrijfswagen.</p>
 			          <br>
 			        </div>
 			      </div>
 			      <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-heart"></i></a> </div>
-			          <h5>Mode de vie sain</h5>
-			          <p>Gardez la forme et votre ligne en pratiquant le vélo.</p>
+			          <h5 class="fr">Mode de vie sain</h5>
+			          <h5 class="en">Healthy lifestyle</h5>
+			          <h5 class="nl">Gezonde levensstijl</h5>
+			          <p class="fr">Gardez la forme et votre ligne en pratiquant le vélo.</p>
+			          <p class="en">Keep in shape and fit while cycling.</p>
+			          <p class="nl">Blijf in vorm en fit als fietser.</p>
 			          <br>
 			        </div>
 			      </div>
 			    </div>
 			    
-			     <h3 class="text-green">Pourquoi travailler avec KAMEO Bikes?</h3>
+			     <h3 class="text-green" "fr">Pourquoi travailler avec KAMEO Bikes ?</h3>
+			     <h3 class="text-green" "en">Why work with KAMEO Bikes?</h3>
+			     <h3 class="text-green" "nl">Waarom werken met KAMEO Bikes?</h3>
 				<div class="row">
 				
 			      <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-cogs"></i></a> </div>
-			          <h5>Vélos de qualité et fiables</h5>
-			          <p>Vos vélos sont entretenus par nos soins régulièrement et sont assurés contre la casse et le vol.</p>
+			          <h5 class="fr">Vélos de qualité et fiables</h5>
+			          <h5 class="en">Quality and reliable bikes</h5>
+			          <h5 class="nl">Kwaliteit en betrouwbare fietsen</h5>
+			          <p class="fr">Vos vélos sont entretenus par nos soins régulièrement et sont assurés contre la casse et le vol.</p>
+			          <p class="en">We regularly maintain your bikes and they are insured against breakage and theft.</p>
+			          <p class="nl">We onderhouden uw fietsen regelmatig en ze zijn verzekerd tegen breuk en diefstal.</p>
 			          <br>
 			        </div>
 			      </div>
@@ -104,9 +154,15 @@ include 'include/header5.php';
 			      <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-sliders"></i></a> </div>
-			          <h5>Offre et services adaptés</h5>
-			          <p>Chaque client a ses propres besoins.<br>
+			          <h5 class="fr">Offre et services sur mesure</h5>
+			          <h5 class="en">Tailor-made offer and services</h5>
+			          <h5 class="nl">Op maat gemaakt aanbod en diensten</h5>
+			          <p class="fr">Chaque client a ses propres besoins.<br>
 			          Toutes nos offres sont personnalisées et correspondent à vos besoins.</p>
+					  <p class="en">Each client has their own needs.<br>
+			          All our offers are customized and match your needs.</p>
+					  <p class="nl">Elke klant heeft zijn eigen behoeften.<br>
+			          Al onze aanbiedingen zijn op maat gemaakt en komen overeen met uw behoeften.</p>
 			          <br>
 			        </div>
 			      </div>
@@ -114,8 +170,12 @@ include 'include/header5.php';
 			       <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-home"></i></a> </div>
-			          <h5>One stop shop</h5>
-			          <p>Vous retrouvez tout pour vos vélos de société au même endroit.</p>
+			          <h5 class="fr">One Stop Shop</h5>
+			          <h5 class="en">One Stop Shop</h5>
+			          <h5 class="nl">One Stop Shop</h5>
+			          <p class="fr">Vous retrouvez tout pour vos vélos de société au même endroit.</p>
+			          <p class="en">You can find everything for your company bikes in one place.</p>
+			          <p class="nl">U vindt alles voor uw bedrijfsfietsen op één plaats.</p>
 			          <br>
 			        </div>
 			      </div>
@@ -123,8 +183,12 @@ include 'include/header5.php';
 			      <div class="col-md-6">
 			        <div class="icon-box box-type effect medium center color">
 			          <div class="icon"> <a href="#"><i class="fa fa-cogs"></i></a> </div>
-			          <h5>Gestion simplifiée</h5>
-			          <p>L'ensemble des services sont gérés par KAMEO Bikes.</p>
+			          <h5 class="fr">Gestion simplifiée</h5>
+			          <h5 class="en">Simplified management</h5>
+			          <h5 class="nl">Vereenvoudigd beheer</h5>
+			          <p class="fr">L'ensemble des services sont gérés et réalisés par KAMEO Bikes.</p>
+			          <p class="en">Everything is managed and performerd by KAMEO Bikes.</p>
+			          <p class="nl">Alles wordt beheerd en uitgevoerd door KAMEO Bikes.</p>
 			          <br>
 			        </div>
 			      </div>
