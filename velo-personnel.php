6a24ef83eb85a445880a75258d10134df46c08b4
Pyadant <63552475+Pyadant@users.noreply.github.com>
2020-04-29 22:48:03 +0200

    Update velo-personnel.php
    Traduction-Framework

--- velo-personnel.php
+++ velo-personnel.php
@@ -6,25 +6,53 @@ include 'include/header5.php';
 			<!-- SECTION SOLUTIONS -->
 			<section class="" style="background: url('images/fond_degrade.jpg');" data-stellar-background-ratio="0.3">
 				<div class="container">
-				<a class="button black-light button-3d effect fill-vertical right"  href="velo-partage.php"><span>Vélos partagés  <i class="fa fa-arrow-right"></i></span></a>
+				<a class="button black-light button-3d effect fill-vertical right fr"  href="velo-partage.php"><span>Vélos partagés  <i class="fa fa-arrow-right"></i></span></a>
+				<a class="button black-light button-3d effect fill-vertical right en"  href="velo-partage.php"><span>Vélos partagés  <i class="fa fa-arrow-right"></i></span></a>
+				<a class="button black-light button-3d effect fill-vertical right nl"  href="velo-partage.php"><span>Vélos partagés  <i class="fa fa-arrow-right"></i></span></a>
 					<div class="row">
 						<div class="col-md-7">
 							<!-- <hr class="space"> -->
-							<h1 class="text-dark">VÉLOS PERSONNELS</h1>
-							<p class="lead text-light"><i class="fa fa-heart"></i> Des employés en <strong>meilleure santé</strong>.<br>
-							<i class="fa fa-clock-o"></i> Des employés jamais <strong>coincés dans le trafic</strong>.<br>
-							<i class="fa fa-leaf"></i> Une entreprise <strong> plus durable</strong>.<br>
-							<i class="fa fa-user"></i> Un atout pour attirer des <strong>nouveaux talents</strong>.</p>
-							<p class="lead text-dark"><strong>Voilà pourquoi proposer des vélos de société.</strong></p>
+							<h1 class="text-dark fr">VÉLOS PERSONNELS</h1>
+							<h1 class="text-dark en">VÉLOS PERSONNELS</h1>
+							<h1 class="text-dark nl">VÉLOS PERSONNELS</h1>
+							<p class="lead text-light fr"><i class="fa fa-heart"></i> Des employés en <strong>meilleure santé</strong>.<br>
+								<i class="fa fa-clock-o"></i> Des employés jamais <strong>coincés dans le trafic</strong>.<br>
+								<i class="fa fa-leaf"></i> Une entreprise <strong> plus durable</strong>.<br>
+								<i class="fa fa-user"></i> Un atout pour attirer des <strong>nouveaux talents</strong>.</p>
+							<p class="lead text-light en"><i class="fa fa-heart"></i> Des employés en <strong>meilleure santé</strong>.<br>
+								<i class="fa fa-clock-o"></i> Des employés jamais <strong>coincés dans le trafic</strong>.<br>
+								<i class="fa fa-leaf"></i> Une entreprise <strong> plus durable</strong>.<br>
+								<i class="fa fa-user"></i> Un atout pour attirer des <strong>nouveaux talents</strong>.</p>
+							<p class="lead text-light nl"><i class="fa fa-heart"></i> Des employés en <strong>meilleure santé</strong>.<br>
+								<i class="fa fa-clock-o"></i> Des employés jamais <strong>coincés dans le trafic</strong>.<br>
+								<i class="fa fa-leaf"></i> Une entreprise <strong> plus durable</strong>.<br>
+								<i class="fa fa-user"></i> Un atout pour attirer des <strong>nouveaux talents</strong>.</p>
+							<p class="lead text-dark fr"><strong>Voilà pourquoi proposer des vélos de société.</strong></p>
+							<p class="lead text-dark en"><strong>Voilà pourquoi proposer des vélos de société.</strong></p>
+							<p class="lead text-dark nl"><strong>Voilà pourquoi proposer des vélos de société.</strong></p>
 							
-							<p class="lead text-light"><strong>KAMEO Bikes</strong> s’occupe de tout et se déplace chez vous :<br>
+							<p class="lead text-light fr"><strong>KAMEO Bikes</strong> s’occupe de tout et se déplace chez vous :<br>
+							- <strong>conseil</strong> sur le vélo adapté,<br>
+							- <strong>maintenance</strong> sur site,<br>
+							- <strong>assurance</strong> et <strong>assistance</strong> dépannage,<br>
+							- installation d'<strong>infrastructures</strong>.<br>
+							- <strong>location tout inclus</strong> ou <strong>achat</strong>.</p>
+							<p class="lead text-light en"><strong>KAMEO Bikes</strong> s’occupe de tout et se déplace chez vous :<br>
+							- <strong>conseil</strong> sur le vélo adapté,<br>
+							- <strong>maintenance</strong> sur site,<br>
+							- <strong>assurance</strong> et <strong>assistance</strong> dépannage,<br>
+							- installation d'<strong>infrastructures</strong>.<br>
+							- <strong>location tout inclus</strong> ou <strong>achat</strong>.</p>
+							<p class="lead text-light nl"><strong>KAMEO Bikes</strong> s’occupe de tout et se déplace chez vous :<br>
 							- <strong>conseil</strong> sur le vélo adapté,<br>
 							- <strong>maintenance</strong> sur site,<br>
 							- <strong>assurance</strong> et <strong>assistance</strong> dépannage,<br>
 							- installation d'<strong>infrastructures</strong>.<br>
 							- <strong>location tout inclus</strong> ou <strong>achat</strong>.</p>
 							
-							<a class="button black-light button-3d effect fill-vertical scroll-to"  href="#plus"><span>Découvrez notre offre <i class="fa fa-arrow-down"></i></span></a>
+							<a class="button black-light button-3d effect fill-vertical scroll-to fr"  href="#plus"><span>Découvrez notre offre <i class="fa fa-arrow-down"></i></span></a>
+							<a class="button black-light button-3d effect fill-vertical scroll-to en"  href="#plus"><span>Découvrez notre offre <i class="fa fa-arrow-down"></i></span></a>
+							<a class="button black-light button-3d effect fill-vertical scroll-to nl"  href="#plus"><span>Découvrez notre offre <i class="fa fa-arrow-down"></i></span></a>
 						</div>
 						<div class="col-md-4">
 							<img src="images/Solution2.png" class="img-responsive img-rounded" alt="Roue des solutions de KAMEO Bikes">
@@ -67,10 +95,18 @@ include 'include/header5.php';
 					<div class="row">
 						<div class="col-md-6">
 							<!-- <hr class="space"> -->
-							<h1 class="text-green">CHOIX DU VÉLO</h1>
-							<p class="text-justify">Soyez sûr que vos employés seront bien conseillés pour rouler avec le vélo qui leur convient. Nos experts prendront le temps d’analyser les besoins de chacun.</p>				
-							<p class="text-justify">Nous trouverons le vélo (électrique ou mécanique) ainsi que les accessoires dont vos employés ont besoin parmi notre catalogue.</p>
-							<p class="text-justify">Envie de brander vos vélos, c’est comme si c’était fait.</p>
+							<h1 class="text-green fr">CHOIX DU VÉLO</h1>
+							<h1 class="text-green en">CHOIX DU VÉLO</h1>
+							<h1 class="text-green nl">CHOIX DU VÉLO</h1>
+							<p class="text-justify fr">Soyez sûr que vos employés seront bien conseillés pour rouler avec le vélo qui leur convient. Nos experts prendront le temps d’analyser les besoins de chacun.</p>				
+							<p class="text-justify en">Soyez sûr que vos employés seront bien conseillés pour rouler avec le vélo qui leur convient. Nos experts prendront le temps d’analyser les besoins de chacun.</p>				
+							<p class="text-justify nl">Soyez sûr que vos employés seront bien conseillés pour rouler avec le vélo qui leur convient. Nos experts prendront le temps d’analyser les besoins de chacun.</p>				
+							<p class="text-justify fr">Nous trouverons le vélo (électrique ou mécanique) ainsi que les accessoires dont vos employés ont besoin parmi notre catalogue.</p>
+							<p class="text-justify en">Nous trouverons le vélo (électrique ou mécanique) ainsi que les accessoires dont vos employés ont besoin parmi notre catalogue.</p>
+							<p class="text-justify nl">Nous trouverons le vélo (électrique ou mécanique) ainsi que les accessoires dont vos employés ont besoin parmi notre catalogue.</p>
+							<p class="text-justify fr">Envie de brander vos vélos, c’est comme si c’était fait.</p>
+							<p class="text-justify en">Envie de brander vos vélos, c’est comme si c’était fait.</p>
+							<p class="text-justify nl">Envie de brander vos vélos, c’est comme si c’était fait.</p>
 							<!--
 							<div class="col-md-12 text-center">
 								<a class="button green button-3d effect fill-vertical" href="achat.php"><span>Le catalogue complet</span></a>
@@ -107,15 +143,23 @@ include 'include/header5.php';
 				<div class="container">
 					<div class="row">
 						<div class="col-md-4">
-						<h1 class="text-green visible-sm visible-xs">MAINTENANCE SUR SITE ET ASSURANCE</h1>
+						<h1 class="text-green visible-sm visible-xs fr">MAINTENANCE SUR SITE ET ASSURANCE</h1>
+						<h1 class="text-green visible-sm visible-xs en">MAINTENANCE SUR SITE ET ASSURANCE</h1>
+						<h1 class="text-green visible-sm visible-xs nl">MAINTENANCE SUR SITE ET ASSURANCE</h1>
 							<img src="images/pvelo.png" class="img-responsive img-rounded" alt="PVELO - l'assurance vélo">
 						</div>
 						<div class="col-md-8">
 							<!-- <hr class="space"> -->
-							<h1 class="text-green visible-md visible-lg">MAINTENANCE SUR SITE ET ASSURANCE</h1>
-							<p class="text-justify">La clé d’une expérience de mobilité réussie est d’avoir en permanence des vélos dans un état irréprochable. C’est pourquoi nous organisons les entretiens sur le site de l’entreprise pendant les heures de travail.</p>
+							<h1 class="text-green visible-md visible-lg fr">MAINTENANCE SUR SITE ET ASSURANCE</h1>
+							<h1 class="text-green visible-md visible-lg en">MAINTENANCE SUR SITE ET ASSURANCE</h1>
+							<h1 class="text-green visible-md visible-lg nl">MAINTENANCE SUR SITE ET ASSURANCE</h1>
+							<p class="text-justify fr">La clé d’une expérience de mobilité réussie est d’avoir en permanence des vélos dans un état irréprochable. C’est pourquoi nous organisons les entretiens sur le site de l’entreprise pendant les heures de travail.</p>
+							<p class="text-justify en">La clé d’une expérience de mobilité réussie est d’avoir en permanence des vélos dans un état irréprochable. C’est pourquoi nous organisons les entretiens sur le site de l’entreprise pendant les heures de travail.</p>
+							<p class="text-justify nl">La clé d’une expérience de mobilité réussie est d’avoir en permanence des vélos dans un état irréprochable. C’est pourquoi nous organisons les entretiens sur le site de l’entreprise pendant les heures de travail.</p>
 
-							<p class="text-justify">Les vélos ont une valeur non négligeable, il est important d’être couvert contre le vol ou toute autre dégradation. Pour cela, KAMEO Bikes collabore avec Aedes et Dedale afin d’offrir à ses clients l’assurance Omnium la plus complète et la plus flexible actuellement disponible sur le marché : <strong class="text-green">La P-Vélo</strong>.
+							<p class="text-justify fr">Les vélos ont une valeur non négligeable, il est important d’être couvert contre le vol ou toute autre dégradation. Pour cela, KAMEO Bikes collabore avec Aedes et Dedale afin d’offrir à ses clients l’assurance Omnium la plus complète et la plus flexible actuellement disponible sur le marché : <strong class="text-green">La P-Vélo</strong>.
+							<p class="text-justify en">Les vélos ont une valeur non négligeable, il est important d’être couvert contre le vol ou toute autre dégradation. Pour cela, KAMEO Bikes collabore avec Aedes et Dedale afin d’offrir à ses clients l’assurance Omnium la plus complète et la plus flexible actuellement disponible sur le marché : <strong class="text-green">La P-Vélo</strong>.
+							<p class="text-justify nl">Les vélos ont une valeur non négligeable, il est important d’être couvert contre le vol ou toute autre dégradation. Pour cela, KAMEO Bikes collabore avec Aedes et Dedale afin d’offrir à ses clients l’assurance Omnium la plus complète et la plus flexible actuellement disponible sur le marché : <strong class="text-green">La P-Vélo</strong>.
 			 
 						</div>
 					</div>
@@ -129,11 +173,19 @@ include 'include/header5.php';
 					<div class="row">
 						<div class="col-md-6">
 							<!-- <hr class="space"> -->
-							<h1 class="text-green">INSTALLATION D'INFRASTRUCTURES</h1>
-							<p class="text-justify">Votre entreprise n'est pas encore équipée d'un parking vélo?<br>
+							<h1 class="text-green fr">INSTALLATION D'INFRASTRUCTURES</h1>
+							<h1 class="text-green en">INSTALLATION D'INFRASTRUCTURES</h1>
+							<h1 class="text-green nl">INSTALLATION D'INFRASTRUCTURES</h1>
+							<p class="text-justify fr">Votre entreprise n'est pas encore équipée d'un parking vélo?<br>
+							KAMEO Bikes, fort de son expérience peut vous conseiller et vous fournir une installation sur mesure pour votre projet.</p>
+							<p class="text-justify en">Votre entreprise n'est pas encore équipée d'un parking vélo?<br>
+							KAMEO Bikes, fort de son expérience peut vous conseiller et vous fournir une installation sur mesure pour votre projet.</p>
+							<p class="text-justify nl">Votre entreprise n'est pas encore équipée d'un parking vélo?<br>
 							KAMEO Bikes, fort de son expérience peut vous conseiller et vous fournir une installation sur mesure pour votre projet.</p>
 			
-							<p class="text-justify">N'hésitez pas à nous demander une remise de prix.</p>
+							<p class="text-justify fr">N'hésitez pas à nous demander une remise de prix.</p>
+							<p class="text-justify en">N'hésitez pas à nous demander une remise de prix.</p>
+							<p class="text-justify nl">N'hésitez pas à nous demander une remise de prix.</p>
 			 
 						</div>
 						<div class="col-md-6">
@@ -149,9 +201,15 @@ include 'include/header5.php';
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12 text-center">
-							<h1 class="text-green">LOCATION TOUT INCLUS OU ACHAT?</h1>
-							<p>Vous choisirez la formule qui vous convient le mieux.</p>
-							<a class="button green button-3d effect fill-vertical" href="location-tout-inclus.php"><span><i class="fa fa-balance-scale"></i>Comparer</span></a>
+							<h1 class="text-green fr">LOCATION TOUT INCLUS OU ACHAT?</h1>
+							<h1 class="text-green en">LOCATION TOUT INCLUS OU ACHAT?</h1>
+							<h1 class="text-green nl">LOCATION TOUT INCLUS OU ACHAT?</h1>
+							<p class="fr">Vous choisirez la formule qui vous convient le mieux.</p>
+							<p class="en">Vous choisirez la formule qui vous convient le mieux.</p>
+							<p class="nl">Vous choisirez la formule qui vous convient le mieux.</p>
+							<a class="button green button-3d effect fill-vertical fr" href="location-tout-inclus.php"><span><i class="fa fa-balance-scale"></i>Comparer</span></a>
+							<a class="button green button-3d effect fill-vertical en" href="location-tout-inclus.php"><span><i class="fa fa-balance-scale"></i>Compare</span></a>
+							<a class="button green button-3d effect fill-vertical nl" href="location-tout-inclus.php"><span><i class="fa fa-balance-scale"></i>Vergelijk</span></a>
 						</div>
 					</div>
 				</div>
@@ -161,10 +219,18 @@ include 'include/header5.php';
 			<!-- CALL TO ACTION -->
 				<div class="jumbotron jumbotron-center jumbotron-fullwidth text-light" style="background: url('images/fond_degrade3.jpg');" data-stellar-background-ratio="0.3">
 				  <div class="container">
-				    <h3>DEMANDEZ UN RENDEZ-VOUS</h3>
-				    <p>Envie de pédaler avec nous?<br>
+				    <h3 class="fr">DEMANDEZ UN RENDEZ-VOUS</h3>
+				    <h3 class="en">DEMANDEZ UN RENDEZ-VOUS</h3>
+				    <h3 class="nl">DEMANDEZ UN RENDEZ-VOUS</h3>
+				    <p class="fr">Envie de pédaler avec nous?<br>
 						Contactez-nous, nous viendrons vous rencontrer dans votre entreprise.</p>
-				    <a class="button large black-light button-3d effect icon-left" href="contact2.php"><span><i class="fa fa-cloud"></i>Nous contacter</span></a> </div>
+					<p class="en">Envie de pédaler avec nous?<br>
+						Contactez-nous, nous viendrons vous rencontrer dans votre entreprise.</p>
+					<p class="nl">Envie de pédaler avec nous?<br>
+						Contactez-nous, nous viendrons vous rencontrer dans votre entreprise.</p>
+				    <a class="button large black-light button-3d effect icon-left fr" href="contact2.php"><span><i class="fa fa-cloud"></i>Nous contacter</span></a> </div>
+				    <a class="button large black-light button-3d effect icon-left en" href="contact2.php"><span><i class="fa fa-cloud"></i>Contact us</span></a> </div>
+				    <a class="button large black-light button-3d effect icon-left nl" href="contact2.php"><span><i class="fa fa-cloud"></i>Contacteer ons</span></a> </div>
 				</div>
 
 			<!--END: CALL TO ACTION -->
