bb6f13e4028560b508fc4fd42a2c06ba1cbd1bb4
Pyadant <63552475+Pyadant@users.noreply.github.com>
2020-05-02 16:25:54 +0200

    Update location-tout-inclus.php

--- location-tout-inclus.php
+++ location-tout-inclus.php
@@ -13,11 +13,10 @@ include 'include/header5.php';
 				<br>
 				<p class="fr">KAMEO Bikes se charge de définir le(s) moyen(s) de mobilité urbaine des entreprises selon leurs besoins, qu'il s'agisse de vélos classiques, électriques, pliants ou même de trottinettes électriques. <br /><br />
                 Toutes les dépenses liées à la mobilité douce sont <strong class="text-green text-uppercase">déductibles à 100%</strong>, n'hésitez donc pas à en profiter ! </p>
-                
-				<p class="en">KAMEO Bikes defines the mobility plan that fits you the best. It can be made of electric, foldable or normal bikes or even electric scooters.<br /><br />
-                All expenses linked to mobility are <strong class="text-green text-uppercase">100% deductible</strong>, make sure you make the most of it!</p>
-				<p class="nl">KAMEO Bikes definieert het mobiliteitsplan dat het beste bij u past. Het kan worden gemaakt van elektrische, opvouwbare of normale fietsen of zelfs elektrische scooters.<br /><br />
-                Alle uitgaven in verband met mobiliteit zijn voor <strong class="text-green text-uppercase">100% aftrekbaar</strong>, zorg dat u er het beste van maakt!</p>
+                <p class="en">KAMEO Bikes defines the mobility plan that fits you the best. It can be made of electric, foldable or mechanical bikes or even electric scooters.<br /><br />
+                All expenses linked to soft mobility are <strong class="text-green text-uppercase">100% deductible</strong>, make sure you make the most of it!</p>
+				<p class="nl">KAMEO Bikes bepaalt het mobiliteitsplan dat het beste bij u past. Het wordt opgebouwd van elektrische, opvouwbare ofwel mechanische fietsen en zelfs elektrische scooters.<br /><br />
+                Alle kosten in verband met zachte mobiliteit zijn <strong class="text-green text-uppercase">100% aftrekbaar</strong>, zorg dat u er het beste van maakt!</p>
 				
 				<!--
 				<p class="fr">KAMEO Bikes propose 3 solutions afin de mettre à disposition des moyens de mobilité à vos employés :</p>
@@ -48,8 +47,8 @@ include 'include/header5.php';
 				    <div class="col-md-6">
 						<div class="form-group">
 							<label class="valeur fr" for="phone">Entrez le prix du vélo souhaité HTVA:</label>
-							<label class="valeur en" for="phone">Bike buying price (Excluding VAT):</label>
-							<label class="valeur nl" for="phone">Aankoopprijs van een fiets (Ex. BTW):</label>
+							<label class="valeur en" for="phone">Bike buying price (excl. VAT):</label>
+							<label class="valeur nl" for="phone">Fiets aankoopprijs (excl. BTW):</label>
 							<input type="number" class="form-control required" name="prix" value="2000" id="prix" aria-required="true" onChange="updatePrices(this)">
 						</div>
 					</div>
@@ -63,9 +62,9 @@ include 'include/header5.php';
 				              <h4 class="fr">Vente (HTVA)</h4>
 				              <p class="text-muted fr">Paiement immédiat (HTVA)</p>
 				              <h4 class="en">Sell (ex. VAT)</h4>
-				              <p class="text-muted en">Immediate buy (excluding VAT)</p>
-				              <h4 class="nl">Aankoop (exclusief BTW)</h4>
-				              <p class="text-muted nl">Onmiddellijke aankoop (exclusief BTW)</p>
+				              <p class="text-muted en">Immediate buy (excl. VAT)</p>
+				              <h4 class="nl">Aankoop (Excl. BTW)</h4>
+				              <p class="text-muted nl">Onmiddellijke aankoop (excl. BTW)</p>
 				              <div class="plan-price" id="retailPrice"></div>
 				            </div>
 				            <div class="plan-list">
@@ -89,7 +88,7 @@ include 'include/header5.php';
                                 </ul>
                                 <ul class="nl">
                                     <li><i class="fa fa-globe"></i>Onbeperkte kilometers</li>
-                                    <li><i class="fa fa-thumbs-up"></i>2 jaar garantie</li>
+                                    <li><i class="fa fa-thumbs-up"></i>Garantie van 2 jaar</li>
                                     <li><i class="fa fa-cogs"></i>On-demand onderhoud <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="top" title="Deze formule omvat niet het onderhoud in de initiële prijs. Het is niettemin mogelijk om er één aan te vragen via het MyKameo-platform. Er wordt een afzonderlijke factuur verzonden."></i></li>
                                     <li><i class="fa fa-lock"></i>On-demand verzekering</li>
                                     <li><i class="fa fa-user"></i>Volledige toegang tot MyKameo</li>
@@ -105,9 +104,9 @@ include 'include/header5.php';
 				            <div class="plan-header">
 				              <h4 class="fr">Location tout inclus (HTVA)</h4>
 				              <p class="text-muted fr">Durée: 3 ans</p>
-				              <h4 class="en">All inclusive rental (ex. VAT)</h4>
+				              <h4 class="en">All inclusive rental (excl. VAT)</h4>
 				              <p class="text-muted en">Contract: 3 years</p>
-				              <h4 class="nl">All-inclusive verhuur (ex. BTW)</h4>
+				              <h4 class="nl">All-inclusive verhuur (excl. BTW)</h4>
 				              <p class="text-muted nl">Contract: 3 jaar</p>
 				              <div class="plan-price fr" id="leasingPriceFR"></div>
 				              <div class="plan-price en" id="leasingPriceEN"></div>
@@ -130,13 +129,13 @@ include 'include/header5.php';
                                     <li><i class="fa fa-lock"></i>Insurance included</li>
                                     <li><i class="fa fa-cogs"></i>4 maintenances over the leasing contract <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="top" title="2 maintenances the first year, one per year afterwards. The exact timing of such maintenance can be configured via MyKameo platform"></i></li>
                                     <li><i class="fa fa-user"></i>Full access to MyKameo</li>
-                                    <li><i class="fa fa-money"></i>End of contract: possibility to buy the bike <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="top" title="At the end of leasing contract, possibility to buy the bike for 15% of its retail price."></i></li>
+                                    <li><i class="fa fa-money"></i>End of contract: option to buy the bike <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="top" title="At the end of leasing contract, possibility to buy the bike for 15% of its retail price."></i></li>
                                     <br>
                                     <a class="button small green button-3d rounded effect icon-left en" data-target="#avantageLeasingPrice" data-toggle="modal" href="#"><span><i class="fa fa-eye"></i>Discover your fiscal advantage</span></a>
 				                </ul>
 				                <ul class="nl">
                                     <li><i class="fa fa-globe"></i>500 kilometer per maand <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="top" title="Het aantal kilometers kan worden opgeteld. Voor een contract van 36 maanden moet het totale aantal kilometers bijvoorbeeld lager zijn dan 36 * 500 = 18.000 km"></i></li>
-                                    <li><i class="fa fa-thumbs-up"></i>2 jaar garantie</li>
+                                    <li><i class="fa fa-thumbs-up"></i>Garantie van 2 jaar</li>
                                     <li><i class="fa fa-lock"></i>Verzekering inbegrepen</li>
                                     <li><i class="fa fa-cogs"></i>4 onderhoud tijdens leasecontract <i class="fa fa-question-circle" data-toggle="tooltip" data-placement="top" title="2 onderhoudt het eerste jaar, één per jaar daarna. De exacte timing van dergelijk onderhoud kan worden geconfigureerd via het MyKameo-platform"></i></li>
                                     <li><i class="fa fa-user"></i>Volledige toegang tot MyKameo</li>
@@ -221,18 +220,18 @@ include 'include/header5.php';
                                             Grâce à l'avantage fiscal, le vélo d'une valeur de <span id="spanHTVARetailPriceFR3"></span> HTVA ne coûte réellement à la société que <strong><span id="spanFinalPriceRetailPriceFR" class="text-green"></span></strong> (<span id="spanHTVARetailPriceFR4"></span> - <span id="spanAvantageFiscalRetailPriceFR2"></span>).
                                             </p>
                                             
-											<p class="en">All expenses linked to green mobility are <strong>100%</strong> deductible, make sure you make the best of it! <br />
+											<p class="en">All expenses linked to soft mobility are <strong>100%</strong> deductible, make sure you make the best of it! <br />
                                             Based on a retail value of <span class="text-green" id="spanRetailPriceEN"></span>, here is the fiscal advantage: <br /><br />
                                             <strong>Retail Price :</strong> <span id="spanRetailPriceEN2"></span><br />
                                             <strong>VAT :</strong> <span id="spanTVARetailPriceEN"></span><br />
-                                            <strong>Price VAT exluded :</strong> <span id="spanHTVARetailPriceEN"></span><br />
+                                            <strong>Price VAT excluded :</strong> <span id="spanHTVARetailPriceEN"></span><br />
                                             <strong>Fiscal advantage :</strong> 100 % x <span id="spanHTVARetailPriceEN2"></span> x 34 % = <span id="spanAvantageFiscalRetailPriceEN" class="text-green"></span><br /><br />
                                             
                                                 
                                             Thanks to the fiscal advantage, bike with a retail price of <span id="spanHTVARetailPriceEN3"></span> (VAT excluded) only costs <strong><span id="spanFinalPriceRetailPriceEN" class="text-green"></span></strong> (<span id="spanHTVARetailPriceEN4"></span> - <span id="spanAvantageFiscalRetailPriceEN2"></span>).
                                             </p>
                                             
-											<p class="nl">Alle uitgaven in verband met groene mobiliteit zijn voor <strong>100%</strong> aftrekbaar, zorg dat u er het beste van maakt! <br />
+											<p class="nl">Alle uitgaven in verband met zachte mobiliteit zijn voor <strong>100%</strong> aftrekbaar, zorg dat u er het beste van maakt! <br />
                                             Gebaseerd op een winkelwaarde van <span class="text-green" id="spanRetailPriceNL"> </span>, is hier het fiscale voordeel:<br /><br />
                                             <strong>Verkoopprijs :</strong> <span id="spanRetailPriceNL2"></span><br />
                                             <strong>BTW :</strong> <span id="spanTVARetailPriceNL"></span><br />
@@ -240,7 +239,7 @@ include 'include/header5.php';
                                             <strong>Fiscaal voordeel :</strong> 100 % x <span id="spanHTVARetailPriceNL2"></span> x 34 % = <span id="spanAvantageFiscalRetailPriceNL" class="text-green"></span><br /><br />
                                             
                                                 
-                                            Dankzij het fiscale voordeel kost een fiets met een verkoopprijs van <span id="spanHTVARetailPriceNL3"> </span> (exclusief btw) alleen <strong><span id="spanFinalPriceRetailPriceNL" class="text-green"></span></strong>
+                                            Dankzij het fiscale voordeel kost een fiets met een verkoopprijs van <span id="spanHTVARetailPriceNL3"> </span> (exclusief BTW) alleen <strong><span id="spanFinalPriceRetailPriceNL" class="text-green"></span></strong>
                                             (<span id="spanHTVARetailPriceNL4" > </span> - <span id="spanAvantageFiscalRetailPriceNL2"> </span>).
                                             </p>
                                             
@@ -292,7 +291,7 @@ include 'include/header5.php';
                                             <strong>Fiscaal voordeel:</strong> 100 % x <span id="spanHTVALeasingPriceNL2"></span> x 34 % = <span id="spanAvantageFiscalLeasingPriceNL" class="text-green"></span><br /><br />
                                             
                                                 
-                                            Dankzij het fiscale voordeel, de leasing met een waarde van <span id="spanHTVALeasingPriceNL3"></span> (Exclusief btw) kost alleen <strong><span id="spanFinalPriceLeasingPriceNL" class="text-green"></span></strong> (<span id="spanHTVALeasingPriceNL4"></span> - <span id="spanAvantageFiscalLeasingPriceNL2"></span>).
+                                            Dankzij het fiscale voordeel, een leasing met een waarde van <span id="spanHTVALeasingPriceNL3"></span> (Exclusief BTW) kost alleen <strong><span id="spanFinalPriceLeasingPriceNL" class="text-green"></span></strong> (<span id="spanHTVALeasingPriceNL4"></span> - <span id="spanAvantageFiscalLeasingPriceNL2"></span>).
                                             </p>
                             
                                             
@@ -342,7 +341,7 @@ include 'include/header5.php';
                                             <strong>Fiscaal voordeel:</strong> 100 % x <span id="spanHTVARentingPriceNL2"></span> x 34 % = <span id="spanAvantageFiscalRentingPriceNL" class="text-green"></span><br /><br />
                                             
                                                 
-                                            Dankzij het fiscale voordeel, de renting met een waarde van <span id="spanHTVARentingPriceNL3"></span> (Exclusief btw) kost alleen <strong><span id="spanFinalPriceRentingPriceNL" class="text-green"></span></strong> (<span id="spanHTVARentingPriceNL4"></span> - <span id="spanAvantageFiscalRentingPriceNL2"></span>).
+                                            Dankzij het fiscale voordeel, de renting met een waarde van <span id="spanHTVARentingPriceNL3"></span> (Exclusief BTW) kost alleen <strong><span id="spanFinalPriceRentingPriceNL" class="text-green"></span></strong> (<span id="spanHTVARentingPriceNL4"></span> - <span id="spanAvantageFiscalRentingPriceNL2"></span>).
                                             </p>
                             
 										</div>
