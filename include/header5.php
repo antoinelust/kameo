da8ee6017efd6bcd004e95048980eac8394eaabf
Antoine Lust <antoine.lust@hotmail.fr>
2020-07-21 09:35:15 +0200

    relative path to find include, images and vendor files to include

--- include/header5.php
+++ include/header5.php
@@ -22,34 +22,34 @@
  	<meta name="author" content="Thibaut Mativa">
  	<meta property="og:image" content="http://www.kameobikes/images/vignette.jpg" />
 
-	<link rel="shortcut icon" href="images/favicon.png">
+	<link rel="shortcut icon" href="../images/favicon.png">
 	<title class="fr">KAMEO Bikes | La solution complète pour vos vélos de société</title>
 	<title class="en">KAMEO Bikes | Bike solutions for businesses</title>
 	<title class="nl">KAMEO Bikes | Fiets oplossingen voor bedrijven</title>
 
 	<!-- Bootstrap Core CSS -->
-	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="vendor/fontawesome/css/font-awesome.min.css" type="text/css" rel="stylesheet">
-	<link href="vendor/animateit/animate.min.css" rel="stylesheet">
+	<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../vendor/fontawesome/css/font-awesome.min.css" type="text/css" rel="stylesheet">
+	<link href="../vendor/animateit/animate.min.css" rel="stylesheet">
 
-	<!-- Vendor css -->
-	<link href="vendor/owlcarousel/owl.carousel.css" rel="stylesheet">
-	<link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+	<!-- ../vendor css -->
+	<link href="../vendor/owlcarousel/owl.carousel.css" rel="stylesheet">
+	<link href="../vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
 	<!-- Template base -->
-	<link href="css/theme-base.css" rel="stylesheet">
+	<link href="../css/theme-base.css" rel="stylesheet">
 
 	<!-- Template elements -->
-	<link href="css/theme-elements.css" rel="stylesheet">
+	<link href="../css/theme-elements.css" rel="stylesheet">
     <!-- Template notifications -->
-	<link href="css/notifications.css" rel="stylesheet">
+	<link href="../css/notifications.css" rel="stylesheet">
 
     <!-- DateTimePicker css -->
-    <link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+    <link href="../css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
 
 
 <!-- Responsive classes -->
-	<link href="css/responsive.css" rel="stylesheet">
+	<link href="../css/responsive.css" rel="stylesheet">
 
 <!--[if lt IE 9]>
 		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
@@ -57,17 +57,17 @@
 
 
 	<!-- Template color -->
-	<link href="css/color-variations/blue.css" rel="stylesheet" type="text/css" media="screen" title="blue">
+	<link href="../css/color-variations/blue.css" rel="stylesheet" type="text/css" media="screen" title="blue">
 
 	<!-- LOAD GOOGLE FONTS -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,800,700,600%7CRaleway:100,300,600,700,800" rel="stylesheet" type="text/css" />
 
 	<!-- CSS CUSTOM STYLE -->
-    <link rel="stylesheet" type="text/css" href="css/custom.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../css/custom.css" media="screen" />
 
     <!--VENDOR SCRIPT-->
-    <script src="vendor/jquery/jquery-1.11.2.min.js"></script>
-    <script src="vendor/plugins-compressed.js"></script>
+    <script src="../vendor/jquery/jquery-1.11.2.min.js"></script>
+    <script src="../vendor/plugins-compressed.js"></script>
 
 	<!-- I am not a robot script -->
 	<script src='https://www.google.com/recaptcha/api.js'></script>
@@ -134,7 +134,7 @@
         <div class="container">
           <!--LOGO-->
           <div id="logo">
-            <a href="/"><img src="images/logo.png" alt="KAMEO Bikes Logo"></a>
+            <a href="/"><img src="../images/logo.png" alt="KAMEO Bikes Logo"></a>
           </div>
           <!--END: LOGO-->
           <!--MOBILE MENU -->
