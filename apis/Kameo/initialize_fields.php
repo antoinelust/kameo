ada8e8ba30046d1158083d12a5df5ba2b1d08ca3
Cyp <the.hacker.37000@gmail.com>
2020-08-07 18:38:49 +0200

    initialize_fields refactoring

--- apis/Kameo/initialize_fields.php
+++ apis/Kameo/initialize_fields.php
@@ -5,24 +5,24 @@ header('Content-type: application/json');
 
 session_start();
 include 'globalfunctions.php';
+include 'connexion.php';
 
 $type=isset($_GET['type']) ? $_GET['type'] : NULL;
 
 if($type=="ownerField"){    
-    global $conn;
-    include 'connexion.php';
     $stmt = $conn->prepare("SELECT OWNER as email, bb.NOM as name, bb.PRENOM as firstName from company_actions aa, customer_referential bb where aa.OWNER=bb.EMAIL and bb.STAANN != 'D' GROUP BY OWNER");
     if($stmt)
     {
         $stmt->execute();
-        echo json_encode($stmt->get_result()->fetch_all(MYSQLI_ASSOC));
-        echo json_encode(Array("reponse" => "success"));
+		$response['owner'] = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+		$response['ownerNumber'] = count($response['owner']);
+		$response['response'] = "success";
+        echo json_encode($response);
         $stmt->close();
-        $conn->close();
     }
     else
-        error_message('500', 'Unable to retrieve Actions owners');
-}else{
+        error_message('500', 'Unable to retrieve actions owners');
+}else
     errorMessage("ES0012");
-}
+$conn->close();
 ?>
