cc6cbb7a21de8798dc3462a1799812a39aa64232
aberkane younes <y.aberkane@student.uliege.be>
2020-09-09 10:18:08 +0200

    correction popup tb catalogue

--- include/tb_popup.php
+++ include/tb_popup.php
@@ -4,12 +4,12 @@
         <div class="row">
             <div class="col-md-12 popup-head">
                 <i id="popup-closeBtn" class="far fa-times-circle"></i>
-                <img src="/images/logotb.png" alt="logo-tb-velo-electrique">
+                <img src="/images/logotb_white.png" alt="logo-tb-velo-electrique">
             </div>
             <div class="col-md-12 popup-body">
                 <h3>Vous cherchez un vélo en tant que particulier ?</h1>
                     <p>Découvrez Test & Buy Vélo Electrique, Notre site vélos pour particuliers !</p>
-                    <a class="row button rounded black-light button-3d effect icon-left" style="background-color: #f7ed6c; display:block; color: #0c0c0c; text-align: center;" href="https://www.tb-velo-electrique.be/"><span><i class="fas fa-arrow-right"></i>Me Rediriger</span></a>
+                    <a class="row button rounded black-light button-3d effect icon-left" style="background-color: #f6e948; display:block; color: #0c0c0c; text-align: center;" href="https://www.tb-velo-electrique.be/"><span><i class="fas fa-arrow-right"></i>Me Rediriger</span></a>
             </div>
 
         </div>
