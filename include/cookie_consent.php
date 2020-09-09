d5ad0ee7e3fc9f3a8facb0edb92dfaf2c75ca002
aberkane younes <y.aberkane@student.uliege.be>
2020-09-09 13:23:33 +0200

    removed "refuse cookies" button to allow utilisation of our plugins (popup, etc)

--- include/cookie_consent.php
+++ include/cookie_consent.php
@@ -1,9 +1,7 @@
 <!-- COOKIE CONSENT -->
 <div class="consent-bar text-center">
     <span> KameoBikes utilise des cookies ou des technologies similaires afin de rendre votre expérience
-        utilisateur optimale.</span>
+        utilisateur fluide et optimale.</span>
     <button class="cookie-btn consent-btn button small green button-3d effect icon-left">Accepter</button>
-    <button class="cookie-btn refuse-btn button small red-dark button-3d effect icon-left">Refuser</button>
 </div>
-
 <!-- END: COOKIE CONSENT -->
--- js/cookie_consent.js
+++ js/cookie_consent.js
@@ -3,18 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
       const consentBar = document.querySelector(".consent-bar");
       const consentBtn = document.querySelector(".consent-btn");
-      const refuseBtn = document.querySelector(".refuse-btn");
 
       consentBar.classList.add("active");
 
       consentBtn.addEventListener("click", () => {
         consentBar.classList.remove("active");
         localStorage.setItem("ConsentAuthorization", "accepted");
-      });
-
-      refuseBtn.addEventListener("click", () => {
-        consentBar.classList.remove("active");
-        localStorage.setItem("ConsentAuthorization", "refused");
       });
     }, 2000);
   }
