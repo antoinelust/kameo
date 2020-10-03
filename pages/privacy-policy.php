c72686993eb8afa33c6882a56c52eca4a5e5f6ae
Mativa <thibaut.mativa@kameobikes.com>
2020-10-03 14:51:47 +0200

    Ajout Conditions générales

--- pages/privacy-policy.php
+++ pages/privacy-policy.php
@@ -17,18 +17,17 @@
 				<h1 class="text-green">Politique de confidentialité</h1>
 				<br>
 				<p class="text-justify text-dark" style="font-size: 18px;">
-                </br></br>
-                <strong>TB vélo électrique</strong> is part of <strong>KAMEO Bikes.</strong> You will therefore see KAMEO Bikes listed in the document below.</br></br>
+                </br>
                 At KAMEO Bikes, we are committed to protecting and respecting your privacy.</br></br>
                 This notice describes what personal data we collect, how we will use that data and how we keep your personal data safe.</br></br>
                 This policy applies to the personal data that we process about you as a natural person. This policy does not apply if we process anonymous or business data.</br></br>
-                If you have any question that is not answered here, or if you would like additional information regarding how we collect, use and store your personal data, you may contact us at info@tb-velo-electrique.com</br></br>
+                If you have any question that is not answered here, or if you would like additional information regarding how we collect, use and store your personal data, you may contact us at info@kameobikes.com</br></br>
 
                 KAMEO Bikes is compliant with the EU General Data Protection Regulation (GDPR) as well as with other national data protection laws of the member states.</br></br>
                 KAMEO BIKES SPRL has its office registered at Rue de la brasserie, 8, 4000 Liège, Belgium. Company number 0681.879.712, VAT number BE0681.879.712.</br>
-                E-mail info@tb-velo-electrique.com</br></br>
+                E-mail info@kameobikes.com</br></br>
 
-                <h3>Plugins</h3></br>
+                <h3 class="text-green">Plugins</h3></br>
                 <p class="text-justify text-dark" style="font-size: 18px;">
                     Social media: Facebook, Twitter, Instagram, Linkedin, …</br>
                     Our webpages contain social plug-ins of social networks. It is possible that personal data about the visitors to the web page are also acquired via these plug-ins, transmitted to the respective service and connected with the visitor’s respective service. After clicking on a social plug-in, the respective supplier obtains the information that you have visited the corresponding page of our online offer. If you do not want the association of your profile with the data supplier, you must log out from your user account before clicking on one of the social plug-ins. Note that KAMEO Bikes has no influence if and to what extent the respective suppliers acquire personal data.</br>
@@ -41,24 +40,24 @@
                         </p>
                     </li>
                 </ul>
-                <h3>Hyperlinks</h3></br>
+                <h3 class="text-green">Hyperlinks</h3></br>
                 <p class="text-justify text-dark" style="font-size: 18px;">
                     Our website has hyperlinks to websites of other suppliers. Upon clicking on these hyperlinks, you will be transferred directly to the website of the other suppliers. Please learn about the privacy and the confidentiality of your personal data directly on these companies website.</br></br>
                 </p>
-                <h3>Data security</h3></br>
+                <h3 class="text-green">Data security</h3></br>
                 <p class="text-justify text-dark" style="font-size: 18px;">
                     KAMEO Bikes is committed to protecting your privacy. We will treat your personal data in all confidentiality. In order to prevent a manipulation or a loss or misuse of your data stored with us, we are taking constant upgrade of our security measure, including integrating new available technologies.</br></br>
                 </p>
-                <h3>Data analysis tools</h3></br>
+                <h3 class="text-green">Data analysis tools</h3></br>
                 <p class="text-justify text-dark" style="font-size: 18px;">
                     We use different analysis tools to assure the continuous optimization of our website. For example, we are able to measure the use of our website by visitors. This allows us to better understand our visitors’ interests and to improve our services.</br></br>
                 </p>
-                <h3>Transfer of data</h3></br>
+                <h3 class="text-green">Transfer of data</h3></br>
                 <p class="text-justify text-dark" style="font-size: 18px;">
                     Our site is fully compliant with the GDPR regulation.</br></br>
                     We will never transfer your personal data to a third party without your prior agreement.</br></br>
                 </p>
-                <h3>Register on our website</h3></br>
+                <h3 class="text-green">Register on our website</h3></br>
                 <p class="text-justify text-dark" style="font-size: 18px;">
                     To purchase bikes or book a test ride on our e-shop, you will have to register with your name, address, e-mail, telephone number, and potentially company detail.</br></br>
                     In case of purchasing, necessary data will be transferred to the shipping partner in charge of delivering the goods. The same will occur with the financial institution entrusted to execute your payment. These companies may use your data only for the purpose of delivering the goods or for executing the transaction and for no other purposes.
