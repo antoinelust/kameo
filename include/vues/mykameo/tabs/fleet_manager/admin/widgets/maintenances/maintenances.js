729419afc1e227376c04476260c195129900ed78
antoinelust <antoine.lust@hotmail.fr>
2020-10-16 10:20:47 +0200

    moving maintenance.js in dedicated folder (better for security)

--- include/vues/mykameo/tabs/fleet_manager/admin/widgets/maintenances/maintenances.js
+++ include/vues/mykameo/tabs/fleet_manager/admin/widgets/maintenances/maintenances.js
@@ -21,6 +21,41 @@ $(".fleetmanager").click(function () {
 
 });
 
+
+$('body').on('click', '.editMaintenance',function(){
+  $("#widget-maintenanceManagement-form div[name=image]").remove();
+  get_maintenance(this.name);
+  $("#widget-maintenanceManagement-form input").attr("readonly", true);
+  $("#widget-maintenanceManagement-form input[name=dateMaintenance]").attr("readonly", false);
+  $("#widget-maintenanceManagement-form select").attr("readonly", false);
+  $("#widget-maintenanceManagement-form textarea").attr("readonly", false);
+  $(".maintenanceManagementTitle").html("Éditer un entretien");
+  $("#widget-maintenanceManagement-form button").show();
+  $("#widget-maintenanceManagement-form div[name=file]").show();
+  $("#widget-maintenanceManagement-form button[name=delete]").show();
+});
+
+$('body').on('click', '.showMaintenance',function(){
+  $("#widget-maintenanceManagement-form div[name=image]").remove();
+  get_maintenance(this.name);
+  $("#widget-maintenanceManagement-form input").attr("readonly", true);
+  $("#widget-maintenanceManagement-form select").attr("readonly", true);
+  $("#widget-maintenanceManagement-form textarea").attr("readonly", true);
+  $(".maintenanceManagementTitle").html("Vu sur un entretien");
+  $("#widget-maintenanceManagement-form button").hide();
+  $("#widget-maintenanceManagement-form div[name=file]").hide();
+  $("#widget-maintenanceManagement-form button[name=delete]").hide();
+});
+
+$('body').on('change', '.form_date_start_maintenance',function(){
+  var dateStart = $(".form_date_start_maintenance").data("datetimepicker").getDate();
+  var dateStartString = ("0" + dateStart.getDate()).slice(-2) + "/" + ("0" + (dateStart.getMonth() + 1)).slice(-2) + "/" + dateStart.getFullYear();
+  $(".form_date_end_maintenance").datetimepicker('setStartDate', dateStartString);
+});
+
+
+
+
 function list_maintenances() {
   var dateStart = $(".form_date_start_maintenance").data("datetimepicker").getDate();
   var dateEnd = $(".form_date_end_maintenance").data("datetimepicker").getDate();
