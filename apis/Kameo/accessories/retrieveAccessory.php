3769574bef2f612bbf270c51c0fa503c1705eb87
JoelleMatar <71333209+JoelleMatar@users.noreply.github.com>
2020-10-16 17:08:55 +0300

    Add accessory

--- apis/Kameo/accessories/retrieveAccessory.php
+++ apis/Kameo/accessories/retrieveAccessory.php
@@ -5,25 +5,25 @@ $ID=isset($_GET['ID']) ? htmlspecialchars($_GET['ID']) : NULL;
 if($ID){
     include '../connexion.php';
 
-    $sql="SELECT * FROM accessories_catalog WHERE ID='$ID'";
+    $sql="SELECT * FROM accessories_catalog, accessories_categories WHERE accessories_catalog.ACCESSORIES_CATEGORIES = accessories_categories.ID AND accessories_catalog.ID='$ID'";
     if ($conn->query($sql) === FALSE) {
         $response = array ('response'=>'error', 'message'=> $conn->error);
         echo json_encode($response);
         die;
     }
+
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
     
     $response['brand']=$row['BRAND'];
     $response['description']=$row['DESCRIPTION'];
-    $response['category']=$row['CATEGORY'];
+    $response['category']=$row['ACCESSORIES_CATEGORIES'];
     $response['provider']=$row['PROVIDER'];
     $response['articleNbr']=$row['ARTICLE_NBR'];
     $response['buyingPrice']=$row['BUYING_PRICE'];
     $response['sellingPrice']=$row['PRICE_HTVA'];
     $response['stock']=$row['STOCK'];
     $response['display']=$row['DISPLAY'];
-
 
 
     $file=__DIR__.'/images_accessories/'.$row['ID'].'jpg';
@@ -34,24 +34,14 @@ if($ID){
     }
     
     
-    
-    $stmt = $conn->prepare("SELECT ID, BRAND, DESCRIPTION, CATEGORY, ACCESSORIES_CATEGORIES, BUYING_PRICE, PRICE_HTVA, STOCK, SHOW_ACCESSORIES,DISPLAY, PROVIDER, ARTICLE_NBR FROM accessories_catalog WHERE ID=?");
-    
+    $stmt = $conn->prepare("SELECT accessories_catalog.ID, accessories_catalog.BRAND, accessories_catalog.DESCRIPTION, accessories_catalog.ACCESSORIES_CATEGORIES, accessories_catalog.BUYING_PRICE, accessories_catalog.PRICE_HTVA, accessories_catalog.STOCK, accessories_catalog.DISPLAY, accessories_catalog.PROVIDER, accessories_catalog.ARTICLE_NBR, accessories_categories.ID, accessories_categories.CATEGORY FROM accessories_catalog, accessories_categories WHERE accessories_catalog.ACCESSORIES_CATEGORIES = accessories_categories.ID AND accessories_catalog.ID=?");
+
     if ($stmt)
     {
         $stmt->bind_param("i", $ID);
         $stmt->execute();
         $response['response']="success";
         $response['accessory']=$stmt->get_result()->fetch_array(MYSQLI_ASSOC);
-
-        /*
-        $file=__DIR__.'/images_accessories/'.$response['ID'].'jpg';
-        if ((file_exists($file))){
-            $response['img']=__DIR__.'/images_accessories/'.$response['ID'];
-        }else{
-        $response['img']=$response['ID'];
-        }   
-        */
 
         $stmt->close();
         echo json_encode($response);
