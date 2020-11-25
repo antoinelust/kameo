565148b0861ce0619232ac60972e6a7bd322b96b
Mativa <thibaut.mativa@kameobikes.com>
2020-11-25 18:31:26 +0100

    Changement du footer comme TB (à valider)

--- include/footer.php
+++ include/footer.php
@@ -4,7 +4,51 @@
         <div class="container">
           <br><br>
           <div class="row text-center">
+			<div class="copyright-text col-md-4">
+				<h3 class="text-green text-bold text-small text-center">Plan du site</h3>
+					<div class="col-md-6 text-center">
+						<a href="https://www.kameobikes.com/">Accueil</a><br>
+						<a href="https://www.kameobikes.com/gestion-flotte">Gestion de flotte</a><br>
+						<a href="https://www.kameobikes.com/cash4bike">Cash For Bike</a><br>
+						<a href="https://www.kameobikes.com/contact">Contact</a>
+					</div>
+					<div class="col-md-6 text-center">
+						<a href="https://www.kameobikes.com/velo-personnel">Vélo personnel</a><br>
+						<a href="https://www.kameobikes.com/achat">Catalogue vélo</a><br>
+						<a href="https://www.kameobikes.com/blog">Blog</a><br>
+						<a href="https://www.kameobikes.com/mykameo">MyKameo</a>
+					</div>
+			</div>
+			
+			<div class="copyright-text col-md-4" style="border-left: 0.5px solid white; border-right: 0.5px solid white">
+				<h3 class="text-green text-bold text-small text-center">Contact</h3>
+					<div class="col-md-12 text-center">
+						<p class="text-light "><i class="fa fa-map-pin"></i> Rue de la Brasserie 8, 4000 Liège<br>
+						<a href="mailto:info@kameobikes.com"><i class="fa fa-envelope-o"></i> info@kameobikes.com</a><br>
+						<a href="tel:0032498727546"><i class="fa fa-phone"></i> +32 498 72 75 46</a><br>
+						<ins>KAMEO BIKES SRL</ins><br>
+						BE 0681.879.712</p>
+					</div>
+			</div>
+			
+			<div class="copyright-text col-md-4">
+				<h3 class="text-green text-bold text-small text-center">Suivez-nous</h3>
+					<div class="col-md-12 text-center">
+						<p class="text-light">Restez au courant de l'actualité de KAMEO Bikes ! </p>
+						<div class="social-icons center">
+						  <ul>
+							<li class="social-facebook"><a href="https://www.facebook.com/Kameo-Bikes-123406464990910/" target="_blank" rel="noopener" aria-label="fb-button"><i class="fa fa-facebook"></i></a></li>
+							<li class="social-linkedin"><a href="https://www.linkedin.com/company/kameobikes/" target="_blank" rel="noopener" aria-label="linkedin-button"><i class="fa fa-linkedin"></i></a></li>
+						  </ul>
+						</div>
+					</div>
+			</div>
+			
+		   </div>
+		   <br><br>
+		  <!--
             <div class="copyright-text text-center">
+
               <ins>Kameo Bikes SPRL</ins>
       				<br>BE 0681.879.712
       				<br>+32 498 72 75 46
@@ -20,6 +64,7 @@
 			  <a href="faq.php" class="text-green text-bold"><h3 class="text-green">FAQ</h3></a>
 		    </div>
 				<br><br>
+			-->
 			<div class="copyright-content">
 			<div class="container">
 				<div class="row">
