19aa3b1adc4124d3edb9bbe70846154a2fa0eada
Mativa <thibaut.mativa@kameobikes.com>
2020-12-05 16:26:06 +0100

    Page de connexion Securex

---
+++ pages/login_securex.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+	include 'include/head.php';
+?>
+<body class="wide">
+	<!-- WRAPPER -->
+	<div class="wrapper">
+		<!--Square icons-->
+	   <!-- SECTION -->
+		<section class="parallax fullscreen" style="background-image: url(/images/bzen2.jpg)">
+			<div class="container container-fullscreen">
+				<div class="text-middle">
+					<div class="row">
+						<div class="col-md-6 center p-30 background-white">
+							<img alt="" src="/images/securex_kameo.jpg">
+							<div class="space"></div>
+							<h3>Connectez-vous avec votre nom d'utilisateur</h3>
+							<form class="form-transparent-grey">
+								<div class="form-group">
+									<label class="sr-only">Adresse mail</label>
+									<input type="text" class="form-control" placeholder="Adresse mail">
+								</div>
+								<div class="form-group m-b-5">
+									<label class="sr-only">Mot de passe</label>
+									<input type="password" class="form-control" placeholder="Mot de passe">
+								</div>
+								<div class="space"></div>
+								<div class="text-right form-group">
+									<button type="button" class="btn btn-primary">Accéder</button><br>
+									<a href="#" class="right"><small>Vous avez oublié votre mot de passe ?</small></a>	
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- END: SECTION -->
+
+	</div>
+	<!-- END: WRAPPER -->
+
+	<!-- Theme Base, Components and Settings -->
+	<script src="js/theme-functions.js"></script>
+
+	<!-- Language management -->
+	<script type="text/javascript" src="js/language.js"></script>
+
+
+</body>
+
+</html>
