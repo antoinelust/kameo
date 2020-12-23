cf43388359701aa0977e6e580a4d9784f7c7e1d2
antoinelust <antoine.lust@hotmail.fr>
2020-12-23 18:31:32 +0100

    problème traduction

--- pages/catalogue/achat.php
+++ pages/catalogue/achat.php
@@ -54,7 +54,6 @@ include 'include/head.php';
                                         <option data-filter=".hnf"><?= L::achat_brand_option7; ?></option>
                                         <option data-filter=".kayza"><?= L::achat_brand_option8; ?></option>
                                         <option data-filter=".moustache">Moustache</option>
-                                        <option data-filter=".orbea"><?= L::achat_brand_option9; ?></option>
                                         <option data-filter=".victoria"><?= L::achat_brand_option10; ?></option>
                                     </select>
                                 </div>
