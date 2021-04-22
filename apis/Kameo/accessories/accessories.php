723680305dcb4bd0c42f0d6276573e6b11b8f961
antoinelust <antoine.lust@hotmail.fr>
2021-04-22 10:11:41 +0200

    Update accessories.php

--- apis/Kameo/accessories/accessories.php
+++ apis/Kameo/accessories/accessories.php
@@ -28,7 +28,7 @@ switch($_SERVER["REQUEST_METHOD"])
 		$id=isset($_GET['ID']) ? $_GET['ID'] : NULL;
 
 		if(get_user_permissions("admin", $token)){
-			$retrieveOrderAcessories= execSQL("SELECT aa.PRICE_HTVA,aa.TYPE,aa.DESCRIPTION,bb.EMAIL, bb.PORTFOLIO_ID, bb.COMPANY,cc.BRAND,cc.MODEL,dd.COMPANY_NAME FROM order_accessories aa,client_orders bb, bike_catalog cc,companies dd WHERE bb.ID=aa.ORDER_ID AND cc.ID=bb.PORTFOLIO_ID AND dd.ID=bb.COMPANY AND  aa.ID='$id'", array(), false);	
+			$retrieveOrderAcessories= execSQL("SELECT aa.PRICE_HTVA,aa.TYPE,aa.DESCRIPTION,bb.EMAIL, bb.PORTFOLIO_ID, bb.COMPANY,cc.BRAND,cc.MODEL,dd.COMPANY_NAME FROM order_accessories aa,client_orders bb, bike_catalog cc,companies dd WHERE bb.ID=aa.ORDER_ID AND cc.ID=bb.PORTFOLIO_ID AND dd.ID=bb.COMPANY AND  aa.ID='$id'", array(), false);
 			$retrieveOrderAcessories['response']="success";
 			echo json_encode($retrieveOrderAcessories);
 			die;
@@ -39,9 +39,9 @@ switch($_SERVER["REQUEST_METHOD"])
 	}
 	else if ($action === 'listOrderAcessories'){
 		if(get_user_permissions("admin", $token)){
-			$listOrderAcessories= execSQL("SELECT aa.*,cc.CATEGORY,ee.BRAND,ee.MODEL,bb.EMAIL, bb.PORTFOLIO_ID,bb.COMPANY, dd.COMPANY_NAME, ff.CONTRACT_TYPE FROM order_accessories aa, accessories_categories cc,client_orders bb, companies dd, bike_catalog ee, accessories_stock ff WHERE ee.ID=bb.PORTFOLIO_ID AND aa.ORDER_ID=bb.ID  AND bb.COMPANY=dd.ID AND cc.ID=aa.CATEGORY AND ff.ORDER_ID=aa.ID 
-				UNION ALL 
-				(SELECT aa.*,cc.CATEGORY,ee.BRAND,ee.MODEL,bb.EMAIL, bb.PORTFOLIO_ID,bb.COMPANY, dd.COMPANY_NAME, ff.CONTRACT_TYPE FROM order_accessories aa, accessories_categories cc,client_orders bb, companies dd, bike_catalog ee, accessories_stock ff WHERE ee.ID=bb.PORTFOLIO_ID AND aa.ORDER_ID=bb.ID  AND bb.COMPANY=dd.ID AND cc.ID=aa.CATEGORY AND not EXISTS(SELECT 1 FROM accessories_stock WHERE ORDER_ID=aa.ID))", array(), false);
+			$listOrderAcessories= execSQL("SELECT aa.*,cc.CATEGORY,ee.BRAND,ee.MODEL,bb.EMAIL, bb.PORTFOLIO_ID,bb.COMPANY, dd.COMPANY_NAME, ff.CONTRACT_TYPE FROM order_accessories aa, accessories_categories cc,client_orders bb, companies dd, bike_catalog ee, accessories_stock ff WHERE ee.ID=bb.PORTFOLIO_ID AND aa.ORDER_ID=bb.ID  AND bb.COMPANY=dd.ID AND cc.ID=aa.CATEGORY AND ff.ORDER_ID=aa.ID
+				UNION ALL
+				(SELECT aa.*,cc.CATEGORY,ee.BRAND,ee.MODEL,bb.EMAIL, bb.PORTFOLIO_ID,bb.COMPANY, dd.COMPANY_NAME, 'NONE' as CONTRACT_TYPE FROM order_accessories aa, accessories_categories cc,client_orders bb, companies dd, bike_catalog ee WHERE ee.ID=bb.PORTFOLIO_ID AND aa.ORDER_ID=bb.ID  AND bb.COMPANY=dd.ID AND cc.ID=aa.CATEGORY AND not EXISTS(SELECT 1 FROM accessories_stock WHERE ORDER_ID=aa.ID))", array(), false);
 			echo json_encode($listOrderAcessories);
 			die;
 		}
@@ -54,7 +54,7 @@ switch($_SERVER["REQUEST_METHOD"])
 		if(get_user_permissions("admin", $token)){
 			$response['state']= execSQL("SELECT aa.*, bb.* FROM accessories_stock aa, accessories_catalog bb WHERE aa.ORDER_ID='$id' AND aa.CATALOG_ID=bb.ID", array(), false);
 			$response['response']="success";
-			
+
 			if($response['state']==null){
 				$status="En attente de traitement";
 			}
@@ -107,7 +107,7 @@ switch($_SERVER["REQUEST_METHOD"])
 		if(get_user_permissions("admin", $token)){
 			$response['state']= execSQL("SELECT aa.*, bb.* FROM accessories_stock aa, accessories_catalog bb WHERE aa.ORDER_ID='$id' AND aa.CATALOG_ID=bb.ID", array(), false);
 			$response['response']="success";
-			
+
 			if($response['state']==null){
 				$status="En attente de traitement";
 			}
